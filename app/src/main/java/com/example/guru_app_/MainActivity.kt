84bc1332aacb5468f38ee5d7ad150e776d2cac47
1014y <7787yunji@gmail.com>
2024-07-22 14:53:31 +0900

    7.22 로그인 화면

--- app/src/main/java/com/example/guru_app_/MainActivity.kt
+++ app/src/main/java/com/example/guru_app_/MainActivity.kt
@@ -1,30 +1,30 @@
 package com.example.guru_app_
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lateinit var sign: EditText
-        //lateinit var Signup: EditText
+        // id가 signin인 버튼을 찾습니다.
+        val textView: TextView = findViewById(R.id.signin)
 
-        // 회원가입 버튼
-        sign = findViewById(R.id.signin)
-
-        // 회원가입 버튼 클릭 시, 회원가입 페이지로 이동
-        sign.setOnClickListener {
+        // 버튼 클릭 리스너를 설정합니다.
+        textView.setOnClickListener {
+            // 새로운 인텐트를 생성하여 SignupActivity로 이동합니다.
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
-
 
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
