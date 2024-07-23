0994ee2174845b281944c841f9a4fc60c242cce4
1014y <7787yunji@gmail.com>
2024-07-23 20:53:32 +0900

    7.23 로그인 화면, 회원가입 화면, 메인화면, 검색화면

--- app/src/main/java/com/example/guru_app_/MainActivity.kt
+++ app/src/main/java/com/example/guru_app_/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         // 버튼 클릭 리스너를 설정합니다.
         signin.setOnClickListener {
             // 새로운 인텐트를 생성하여 SignupActivity로 이동합니다.
-            //var할시 오류.. 초기화문제
+            //var할시 오류.. 초기화문제 한번만 사용되기 때문에. 다시 돌아오는경우엔?
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
