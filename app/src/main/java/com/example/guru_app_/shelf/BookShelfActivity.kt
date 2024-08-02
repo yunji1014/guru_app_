1c129bc63adc98e915cc5b63c40a3450053468b2
sohnjiye <sohnjy0819@naver.com>
2024-08-02 20:52:50 +0900

    8/2 네비게이션 바 (bookshelf, arfilter, mypage) 추가 및 mypage db 수정

--- app/src/main/java/com/example/guru_app_/shelf/BookShelfActivity.kt
+++ app/src/main/java/com/example/guru_app_/shelf/BookShelfActivity.kt
@@ -1,9 +1,14 @@
 package com.example.guru_app_.shelf
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.guru_app_.ARFilter
+import com.example.guru_app_.HomeActivity
+import com.example.guru_app_.MyPageActivity
 import com.example.guru_app_.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 //import com.example.guru_app_.database.BookDatabaseHelper
 import com.google.android.material.tabs.TabLayout
 
@@ -34,6 +39,29 @@ class BookShelfActivity : AppCompatActivity() {
 
         // 기본적으로 첫 번째 탭을 선택합니다.
         supportFragmentManager.beginTransaction().replace(R.id.main_view, readingFragment, "READING_FRAGMENT").commit()
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                R.id.navigation_bookshelf -> {
+                    startActivity(Intent(this, BookShelfActivity::class.java))
+                    true
+                }
+                R.id.navigation_arfilter -> {
+                    startActivity(Intent(this, ARFilter::class.java))
+                    true
+                }
+                R.id.navigation_mypage -> {
+                    startActivity(Intent(this, MyPageActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
 
